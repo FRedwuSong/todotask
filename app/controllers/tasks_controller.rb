aca48c2bc1c50457682b59d3d95a82b4f4f27019
FRedwuSong <diadora1166@gmail.com>
2023-10-20 23:44:35 +0800

    disply task 's state on index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -60,7 +60,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :content, :end_time)
+    params.require(:task).permit(:title, :content, :end_time, :state)
   end
 
   def find_task
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,20 @@ class Task < ApplicationRecord
   include AASM
 
   aasm column: 'state' do
+    # 待處理 pending
+    state :pending, initial: true
+    # 進行中 process
+    state :process
+    # 完成 done
+    state :completed
+
+    event :processing do
+      transitions from: :pending, to: :process
+    end
+
+    event :completing do
+      transitions from: :process, to: :complete
+    end
   end
   scope :sort_by_created_at_asc, -> { order(created_at: :asc) }
   scope :sort_by_created_at_desc, -> { order(created_at: :desc) }
