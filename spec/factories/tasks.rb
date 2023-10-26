e4dba4441ae3d79c13543bb739f2ba346223ee82
FRedwuSong <diadora1166@gmail.com>
2023-10-26 23:57:18 +0800

    Add featue test

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     title { Faker::TvShows::BigBangTheory.character }
     content { Faker::TvShows::BigBangTheory.quote }
     end_time { DateTime.now + 1.year }
+    priority { [0, 1, 2].sample }
     :state
   end
 end
